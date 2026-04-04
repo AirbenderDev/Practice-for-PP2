5e86629418dcaab3aafcfeb75f96eb51545a3a01
AirbenderDev <kamalbekaltaj@gmail.com>
2026-04-04 09:04:10 +0500

    Add Practice8 - PhoneBook with functions and stored procedures

--- Practice-08/config.py
+++ Practice-08/config.py
@@ -1,5 +1,4 @@
 DB_NAME = "phonebook_db"
-DB_USER = "postgres"
-DB_PASSWORD = "1234"
+DB_USER = "altai"
 DB_HOST = "localhost"
 DB_PORT = "5432"
--- Practice-08/connect.py
+++ Practice-08/connect.py
@@ -6,7 +6,6 @@ def get_conn():
     return psycopg2.connect(
         dbname=DB_NAME,
         user=DB_USER,
-        password=DB_PASSWORD,
         host=DB_HOST,
         port=DB_PORT
     )
